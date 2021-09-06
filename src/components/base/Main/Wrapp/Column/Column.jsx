cdd4f6c17f12f7f964e096ab978d340adb65d6a6
Nataliya1984 <lisa.volma1984@gmail.com>
2021-09-06 19:09:58 +0300

    1111

--- src/components/base/Main/Wrapp/Column/Column.jsx
+++ src/components/base/Main/Wrapp/Column/Column.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import jss from 'jss';
 import preset from 'jss-preset-default';
